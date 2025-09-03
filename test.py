0950af726623868a95d5317c454a6b448ed09d26
Eze1504 <eze.sarabia04@gmail.com>
2025-09-03 08:10:45 -0300

    Integracion continua

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from main import suma 
+from main import resta
+class TestPrueba(unittest.TestCase):
+
+    def test_suma(self):
+        self.assertEqual(suma(2,3),5)
+
+
+
+    def test_resta(self):
+        self.assertEqual(resta(6,3),3)
+
+
+if __name__ == "__main__": 
+    unittest.main()       
